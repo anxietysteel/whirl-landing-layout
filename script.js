6b078e0a4145f7b3f47ff0cc9680e1617620f2dd
Stanley <thestr1keacc@gmail.com>
2025-11-08 22:18:25 +0500

    desktop layout view completion

---
+++ script.js
@@ -0,0 +1,8 @@
+const accordionItems = document.querySelectorAll(".accordion__item");
+
+accordionItems.forEach((item) => {
+  const title = item.querySelector(".accordion__title");
+  title.addEventListener("click", () => {
+    item.classList.toggle("active");
+  });
+});
